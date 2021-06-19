e1c3fb6aa371b98c5a46bf6076deddfa5eae8428
pshashwati <pragya.shashwati2019@vitstudent.ac.in>
2021-06-19 20:54:29 +0530

    updated

---
+++ index.js
@@ -0,0 +1,6 @@
+work = document.getElementById('work')
+work.addEventListener('click',function(){
+    home.style.display = "none";
+    content.style.display = "none";
+    skills.style.display = "block"
+})
